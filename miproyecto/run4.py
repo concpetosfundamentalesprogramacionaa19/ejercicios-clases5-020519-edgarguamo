cf2b2ac914cd73d98e552c085dee7c653485a36d
edgarguamo@gmail.com <edgarguamo@gmail.com>
2019-05-02 10:42:27 -0500

    Clase 5 con ejercicio

--- miproyecto/run4.py
+++ miproyecto/run4.py
@@ -10,12 +10,14 @@ de ciclos a seguir es de 10, caso contrario deberá seguir 8 ciclos.
 Obtener la proyección del costo de la carrera de un estudiante   
 	
 """
+# Mensaje de Bienvenida y petición de datos 
 print ("Bienvenido al generador de costo por carrera de La UTPL")
 modalidad = input ("Por favor ingrese la modalidad que seguirá\n")
 edad = input ("A continuación ingrese la edad\n")
-
+# Conversion de la variable
 edad = int (edad)
 
+# Operación a seguir si la modalidad es presencial
 if (modalidad =="Presencial"):
 	modulos = 8 * 1200
 	if (edad <= 20):
@@ -23,6 +25,7 @@ if (modalidad =="Presencial"):
 	else:
 		seguro = 150 * 8 
 else:
+# Operación a seguir si la modalidad es a distancia 
 	if (modalidad == "Distancia"):
 		modulos = 10 *1200
 		if (edad <=20):
@@ -30,6 +33,7 @@ else:
 		else:
 			seguro = 150 * 8 
 
-
+# suma de los valores 
 total_matricula = seguro + modulos
+# Impresión del resultado 
 print("El valor total a pagar por su carrera es de: ", total_matricula)
